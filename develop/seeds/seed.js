033e8741fafc5ec3c8f11f75aaac43d852d59b72
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-06-18 11:24:51 -0400

    general changes

---
+++ develop/seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const PostData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const Posts of PostData) {
+    await Post.create({
+      ...Posts,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
